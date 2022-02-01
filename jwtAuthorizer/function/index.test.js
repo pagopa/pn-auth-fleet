6e708b3e16d58ef4fc826c433174a14d8babe811
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-02-01 12:57:59 +0100

    Test jwttoken sample

--- jwtAuthorizer/function/index.test.js
+++ jwtAuthorizer/function/index.test.js
@@ -7,6 +7,6 @@ test('Runs function handler', async () => {
     let eventFile = fs.readFileSync('event.json')
     let event = JSON.parse(eventFile)
     let response = await index.handler(event, null)
-    expect(JSON.stringify(response)).toContain('principalId')
+    expect(response.policyDocument.Statement[0].Effect).toBe('Allow')
   }
 )
