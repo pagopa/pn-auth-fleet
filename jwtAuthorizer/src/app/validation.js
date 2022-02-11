9ebd220f64ce7c34bf2be6ba3c26deeadd833e29
Alessandro Masci <alessandro.masci@pagopa.it>
2022-02-11 12:14:46 +0100

    add kms public key retrieve

--- jwtAuthorizer/src/app/validation.js
+++ jwtAuthorizer/src/app/validation.js
@@ -3,13 +3,14 @@ const fetch = require('node-fetch');
 const AWS = require("aws-sdk");
 const kms = new AWS.KMS();
 const jsonwebtoken = require('jsonwebtoken');
-var ValidationException = require('./exception/validationException.js');
+const ValidationException = require('./exception/validationException.js');
+//const cachedPublicKey = {};
 
 module.exports = {
     async validation (jwtToken, cachedPublicKey){
         if(jwtToken){
             let decodedToken = await jwtValidator(jwtToken, cachedPublicKey);
-            console.info('token is valid');
+            console.log('token is valid');
             return decodedToken;    
         }else{
             throw new ValidationException("token is not valid")
@@ -20,16 +21,19 @@ module.exports = {
 
 async function jwtValidator(jwtToken, cachedPublicKey) {
     const token = jsonwebtoken.decode(jwtToken, { complete: true });
-    console.log('token', token)
-    let publicKey;
+    console.log('token ', token)
+    let publicKey
     if (cachedPublicKey && cachedPublicKey.expiresOn > Date.now()) {
+        console.log( 'Using cached public key' )
         publicKey = cachedPublicKey.value;
     } else {
         publicKey = await retrievePublicKey();
         setCachedData(publicKey)
     }
     try{
-        jsonwebtoken.verify(jwtToken, publicKey)
+        let publicKeyPem = '-----BEGIN PUBLIC KEY-----\n' + publicKey.PublicKey + '\n-----END PUBLIC KEY-----'
+        console.debug( 'publicKeyPem', publicKeyPem )
+        jsonwebtoken.verify(jwtToken, publicKeyPem)
     }catch(err){
         console.error('Validation error ', err)
         throw new ValidationException(err.message)
@@ -39,6 +43,7 @@ async function jwtValidator(jwtToken, cachedPublicKey) {
 }
 
 const setCachedData = (val) =>{
+    console.debug( 'Set cached public key' )
     cachedPublicKey = {
         expiresOn: Date.now() + 3600 * 1000, // Set expiry time of 1H
         value: val
@@ -46,10 +51,11 @@ const setCachedData = (val) =>{
 }
 
 async function retrievePublicKey() {
+    console.debug( 'Retrieving public key from KMS' )
     let res = kms.getPublicKey({
         KeyId: process.env.KEY_ID 
     }).promise()
 
-    console.log('res');
-    return res.PublicKey;
+    console.debug('res', res);
+    return res;
 }
