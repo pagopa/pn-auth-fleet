ea7c040bac4cd2ce6a6e49efa9c82ce27d866a5d
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-03-01 11:56:03 +0100

    PN-782: fix pullrequest params

--- jwtAuthorizer/sonarqube-scanner.js
+++ jwtAuthorizer/sonarqube-scanner.js
@@ -6,6 +6,9 @@ scanner(
     options: {
       "sonar.organization": "pagopa",
       "sonar.projectKey": "pagopa_pn-auth-fleet_jwtAuthorizer",
+      "sonar.pullrequest.base": process.env.BRANCH_TARGET,
+      "sonar.pullrequest.branch": process.env.BRANCH_NAME,
+      "sonar.pullrequest.key": process.env.PR_NUM
     },
   },
   () => process.exit()
