aa9fab3f1516802ebdbdd03b6bbcf8cee799ebd2
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-03-07 18:29:47 +0100

    Feature/pn 884 bug sonar

--- jwtAuthorizer/sonarqube-scanner.js
+++ jwtAuthorizer/sonarqube-scanner.js
@@ -1,20 +1,20 @@
-
-const scanner = require("sonarqube-scanner");
 let options = {
     "sonar.organization": "pagopa",
     "sonar.projectKey": "pagopa_pn-auth-fleet_jwtAuthorizer"
 }
 
-if (process.env.PR_NUM != null) {
+if (type process.env.PR_NUM !== 'undefined' ) {
     options["sonar.pullrequest.base"] = process.env.BRANCH_TARGET;
     options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME;
     options["sonar.pullrequest.key"] = process.env.PR_NUM;
 }
+
+const scanner = require("sonarqube-scanner");
+
 scanner(
   {
     serverUrl: "https://sonarcloud.io",
     options: options
-    },
   },
   () => process.exit()
 );
