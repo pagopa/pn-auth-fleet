78e82d6f6a18f9d31344f7157e8648d19d35866f
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-03-07 17:41:43 +0100

    PN-884: fix bug on sonar analysis

--- apikeyAuthorizer/sonarqube-scanner.js
+++ apikeyAuthorizer/sonarqube-scanner.js
@@ -1,12 +1,15 @@
 
 const scanner = require("sonarqube-scanner");
 
-let options = {}
+let options = {
+    "sonar.organization": "pagopa",
+    "sonar.projectKey": "pagopa_pn-auth-fleet_apikeyAuthorizer"
+}
 
 if (process.env.PR_NUM) {
     options["sonar.pullrequest.base"] = process.env.BRANCH_TARGET;
-    options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME,
-    options["sonar.pullrequest.key"] = process.env.PR_NUM
+    options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME;
+    options["sonar.pullrequest.key"] = process.env.PR_NUN;
 }
 
 scanner(
