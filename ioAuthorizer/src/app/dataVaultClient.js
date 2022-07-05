6e8daaa4565633ef50ce2a4fb17ff0417bac873c
MTurraPpa <94441477+MTurraPpa@users.noreply.github.com>
2022-07-05 17:36:59 +0200

    Fix data vault get to post refactoring

--- ioAuthorizer/src/app/dataVaultClient.js
+++ ioAuthorizer/src/app/dataVaultClient.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 module.exports = {
     async getCxId(taxId) {
         const pnDataVaultBaseUrl = process.env.PN_DATA_VAULT_BASEURL;
-        const pnDataVaultUrl = pnDataVaultBaseUrl + '/datavault-private/v1/recipients/external/PF/' + taxId;
+        const pnDataVaultUrl = pnDataVaultBaseUrl + '/datavault-private/v1/recipients/external/PF';
         try {
-            let response = await axios.get(pnDataVaultUrl);
+            let response = await axios.post(pnDataVaultUrl, taxId, { headers: { 'Content-Type':'text/plain' } });
             return response.data;
         } catch(err){
             console.error('Error in get external Id ', err);
