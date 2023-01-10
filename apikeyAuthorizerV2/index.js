bff8f8cc300399be276f5ef2b1d844e033eac589
empassaro <emanuele.passaro@nttdata.com>
2023-01-10 15:46:36 +0100

    Fixed anonymization of event

--- apikeyAuthorizerV2/index.js
+++ apikeyAuthorizerV2/index.js
@@ -2,6 +2,6 @@ const workflowHandler = require('./src/app/eventHandler.js');
 const utils = require("./src/app/utils.js");
 
 exports.handler = async (event) => {
-    console.info("New event received ", utils.anonymizeEvent(event));
+    utils.logEvent(event);
     return workflowHandler.eventHandler(event);
 };
