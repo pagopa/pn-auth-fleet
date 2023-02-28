137fae5aa5f4d4c98bd7a4d06b2fca02519ab44f
Lorenzo Cocciarelli <100523847+LorenzoCocciarelli@users.noreply.github.com>
2023-02-28 11:23:32 +0100

    feat: adds check on aws account and PG to map organization id from fiscal code (#100)
    * feat: adds check on aws account and PG to map organization id from fiscal code
    
    * fix: removes PG- from fiscalcode to ids object

--- tokenExchange/src/app/eventHandler.js
+++ tokenExchange/src/app/eventHandler.js
@@ -1,11 +1,15 @@
-const validator = require('./validation.js')
-const tokenGen = require('./tokenGen.js')
-const responses = require('./responses.js')
+const validator = require('./validation.js');
+const tokenGen = require('./tokenGen.js');
+const responses = require('./responses.js');
 const auditLog = require("./log.js");
-const utils = require("./utils.js")
+const utils = require("./utils.js");
 
 module.exports = {
-    async handleEvent(event){
+    async handleEvent(event, context){
+        const awsAccountId = context?.invokedFunctionArn?.split(":")[4];
+        console.log("AWS Account ID", awsAccountId);
+        const isDev = awsAccountId === "558518206506";
+
         event.headers = utils.makeLower(event.headers);
         const eventOrigin = event?.headers?.origin;
         if (eventOrigin) {
@@ -25,7 +29,7 @@ module.exports = {
                     try {
                         const decodedToken = await validator.validation(encodedToken);
                         const enrichedToken = utils.enrichDecodedToken(decodedToken);
-                        const sessionToken = await tokenGen.generateToken(enrichedToken);
+                        const sessionToken = await tokenGen.generateToken(enrichedToken, isDev);
                         const uid = enrichedToken.uid;
                         const cx_id = enrichedToken.organization ? enrichedToken.organization.id : ('PF-' + enrichedToken.uid);
                         const cx_type = utils.getUserType(enrichedToken);
