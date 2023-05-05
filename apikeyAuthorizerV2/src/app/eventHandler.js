345cf7ca766ecf604bb87760a7ee73f26a60cac4
empassaro <emanuele.passaro@nttdata.com>
2023-05-05 12:36:46 +0200

    fix: changed equality comparison for pdnd token

--- apikeyAuthorizerV2/src/app/eventHandler.js
+++ apikeyAuthorizerV2/src/app/eventHandler.js
@@ -1,84 +1,101 @@
-const dynamo = require('./dynamoFunctions.js');
-const { KeyStatusException, ValidationException } = require('./exceptions.js');
-const iam = require('./iamPolicyGenerator.js');
+const dynamo = require("./dynamoFunctions.js");
+const { KeyStatusException, ValidationException } = require("./exceptions.js");
+const iam = require("./iamPolicyGenerator.js");
 const utils = require("./utils");
-const validator = require('./validation.js')
+const validator = require("./validation.js");
 
 const defaultDenyAllPolicy = {
-    "principalId": "user",
-    "policyDocument": {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": "execute-api:Invoke",
-                "Effect": "Deny",
-                "Resource": "*"
-            }
-        ]
-    }
+  principalId: "user",
+  policyDocument: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "execute-api:Invoke",
+        Effect: "Deny",
+        Resource: "*",
+      },
+    ],
+  },
 };
 
 module.exports.eventHandler = async (event, context) => {
-    try {
-        const virtualKey = event.headers["x-api-key"];
+  try {
+    const virtualKey = event.headers["x-api-key"];
 
-        const apiKeyDynamo = await dynamo.getApiKeyByIndex(virtualKey);
-        
-        if(!checkStatus(apiKeyDynamo.status)) {
-            throw new KeyStatusException(`Key is not allowed with status ${apiKeyDynamo.status}`);
-        }
+    const apiKeyDynamo = await dynamo.getApiKeyByIndex(virtualKey);
 
-        const paAggregationDynamo = await dynamo.getPaAggregationById(apiKeyDynamo.cxId);
-        console.log("Aggregate ID found -> ", paAggregationDynamo.aggregateId)
-
-        const aggregateDynamo = await dynamo.getPaAggregateById(paAggregationDynamo.aggregateId);
-        console.log("AWS ApiKey Found -> ", utils.anonymizeKey(aggregateDynamo.AWSApiKey));
-
-        let contextAuth;
-        const encodedToken = event?.headers?.Authorization?.replace('Bearer ','');
-        if(apiKeyDynamo.pdnd === false){
-            if(encodedToken){
-                console.warn('PDND Token is not required')
-                throw new ValidationException('PDND Token is not required, you have to use only API-KEY to access this resource')
-            }
-            contextAuth = {
-                "uid": "APIKEY-" + aggregateDynamo.AWSApiKey,
-                "cx_id": apiKeyDynamo.cxId,
-                "cx_groups": apiKeyDynamo?.groups?.join(),
-                "cx_type": "PA"
-            };
-        }else{
-            if (encodedToken) {
-                console.log('encodedToken', encodedToken);
-                let decodedToken = await validator.validation(encodedToken);
-                contextAuth = {
-                    "uid": "PDND-" + decodedToken.client_id,
-                    "cx_id": apiKeyDynamo.cxId,
-                    "cx_groups": apiKeyDynamo?.groups?.join(),
-                    "cx_type": "PA"
-                };
-            } else {
-                console.warn('PDND Token is required')
-                throw new ValidationException('PDND Token is required, you have to use both APIKEY and PDND token to access this resource')
-            }
-        }
-        const iamPolicy = iam.generateIAMPolicy(event.methodArn, contextAuth, aggregateDynamo.AWSApiKey);
-        utils.logIamPolicy(iamPolicy);
-        return iamPolicy;
-    } catch (error) {
-        return handleError(error)
+    if (!checkStatus(apiKeyDynamo.status)) {
+      throw new KeyStatusException(
+        `Key is not allowed with status ${apiKeyDynamo.status}`
+      );
     }
-}; 
+
+    const paAggregationDynamo = await dynamo.getPaAggregationById(
+      apiKeyDynamo.cxId
+    );
+    console.log("Aggregate ID found -> ", paAggregationDynamo.aggregateId);
+
+    const aggregateDynamo = await dynamo.getPaAggregateById(
+      paAggregationDynamo.aggregateId
+    );
+    console.log(
+      "AWS ApiKey Found -> ",
+      utils.anonymizeKey(aggregateDynamo.AWSApiKey)
+    );
+
+    let contextAuth;
+    const encodedToken = event?.headers?.Authorization?.replace("Bearer ", "");
+    if (apiKeyDynamo.pdnd === true) {
+      if (encodedToken) {
+        console.log("encodedToken", encodedToken);
+        let decodedToken = await validator.validation(encodedToken);
+        contextAuth = {
+          uid: "PDND-" + decodedToken.client_id,
+          cx_id: apiKeyDynamo.cxId,
+          cx_groups: apiKeyDynamo?.groups?.join(),
+          cx_type: "PA",
+        };
+      } else {
+        console.warn("PDND Token is required");
+        throw new ValidationException(
+          "PDND Token is required, you have to use both APIKEY and PDND token to access this resource"
+        );
+      }
+    } else {
+      if (encodedToken) {
+        console.warn("PDND Token is not required");
+        throw new ValidationException(
+          "PDND Token is not required, you have to use only API-KEY to access this resource"
+        );
+      }
+      contextAuth = {
+        uid: "APIKEY-" + aggregateDynamo.AWSApiKey,
+        cx_id: apiKeyDynamo.cxId,
+        cx_groups: apiKeyDynamo?.groups?.join(),
+        cx_type: "PA",
+      };
+    }
+    const iamPolicy = iam.generateIAMPolicy(
+      event.methodArn,
+      contextAuth,
+      aggregateDynamo.AWSApiKey
+    );
+    utils.logIamPolicy(iamPolicy);
+    return iamPolicy;
+  } catch (error) {
+    return handleError(error);
+  }
+};
 
 function checkStatus(status) {
-    return status === 'ENABLED' || status === 'ROTATED';
+  return status === "ENABLED" || status === "ROTATED";
 }
 
 function handleError(error) {
-    if(error instanceof KeyStatusException) {
-        console.warn('Error generating IAM policy with error ', error);
-    } else {
-        console.error('Error generating IAM policy with error ', error);
-    }
-    return defaultDenyAllPolicy;
+  if (error instanceof KeyStatusException) {
+    console.warn("Error generating IAM policy with error ", error);
+  } else {
+    console.error("Error generating IAM policy with error ", error);
+  }
+  return defaultDenyAllPolicy;
 }
