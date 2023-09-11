79a7869c2354fe52101173c279690e6c128dc7f7
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-09-11 17:18:24 +0200

    PN-7669: service desk authorizer

---
+++ servicedeskAuthorizer/src/app/exception/validationException.js
@@ -0,0 +1,7 @@
+/* istanbul ignore next */
+module.exports =  class ValidationException extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'ValidationException'
+    }
+}
