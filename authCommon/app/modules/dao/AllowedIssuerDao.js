8e100d5dd1e056dca67a6d299fae880fbde0da22
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-02-08 13:42:07 +0100

    PN-9818: jwksCacheRefreshLambda introduced & test implementation

--- authCommon/app/modules/dao/AllowedIssuerDao.js
+++ authCommon/app/modules/dao/AllowedIssuerDao.js
@@ -1,9 +1,9 @@
 const { ddbDocClient } = require('./DynamoDbClient')
-const { QueryCommand, GetCommand, TransactWriteCommand } = require("@aws-sdk/lib-dynamodb");
+const { QueryCommand, GetCommand, TransactWriteCommand, UpdateItemCommand} = require("@aws-sdk/lib-dynamodb");
 const { CFG, ISS_PREFIX, JWKS_CACHE_PREFIX, JWKS_CACHE_EXPIRE_SLOT_ATTRIBUTE_NAME } = require('./constants');
 const crypto = require('crypto')
 
-function getISSFromHashKey(hashKey){
+async function getISSFromHashKey(hashKey){
     return hashKey.split('~')[1]
 }
 
@@ -153,7 +153,8 @@ async function listJwksCacheExpiringAtMinute(expiringMinute){
         KeyConditionExpression:  JWKS_CACHE_EXPIRE_SLOT_ATTRIBUTE_NAME+' = :expiringMinute',
         ExpressionAttributeValues: {
             ':expiringMinute': expiringMinute
-        }
+        },
+        ConsistentRead: true
     }
 
     const queryCommand = new QueryCommand(queryInput)
@@ -163,9 +164,25 @@ async function listJwksCacheExpiringAtMinute(expiringMinute){
     return result.Items
 }
 
+async function postponeJwksCacheEntryValidation(hashKey, jwksCacheExpireSlot){
+    const updateItemInput = {
+        TableName: process.env.AUTH_JWT_ISSUER_TABLE,
+        Item: {
+            "hashKey": hashKey,
+            "jwksCacheExpireSlot": jwksCacheExpireSlot
+        }
+    }
+
+    const updateItemCommand = new UpdateItemCommand(updateItemInput)
+
+    return await ddbDocClient.send(updateItemCommand)
+}
+
+
 
 module.exports = {
     getIssuerInfoAndJwksCache,
     addJwksCacheEntry,
-    listJwksCacheExpiringAtMinute
+    listJwksCacheExpiringAtMinute,
+    postponeJwksCacheEntryValidation
 }
--- jwksCacheRefreshLambda/src/app/eventHandler.js
+++ jwksCacheRefreshLambda/src/app/eventHandler.js
@@ -1,8 +1,39 @@
+const { AllowedIssuerDao } = require('pn-auth-common')
+const { UrlDownloader } = require('pn-auth-common')
+
 async function handleEvent(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'OK' }),
-  }
+    const date = Date.now();
+    const initialTimeInMillis = date;
+    let pivotTimeInMillis = date;
+    let issuersToRenew = await AllowedIssuerDao.listJwksCacheExpiringAtMinute( transformInDate(pivotTimeInMillis) )
+    while(issuersToRenew.length > 0 || initialTimeInMillis-pivotTimeInMillis < process.env.JWKS_REFRESH_INTERVAL_MINUTES) {
+        for ( allowedIssuerToRenew of issuersToRenew ) {
+            const allowedIssuerId = allowedIssuerToRenew.hashKey.split('~')[1]
+            try {
+                await AllowedIssuerDao.addJwksCacheEntry( allowedIssuerId, UrlDownloader.downloadUrl )
+            }
+            catch {
+                const rescheduleTime = pivotTimeInMillis + (process.env.JWKS_REFRESH_INTERVAL_MINUTES * 1000);
+                console.log(rescheduleTime)
+                await AllowedIssuerDao.postponeJwksCacheEntryValidation( allowedIssuerToRenew.hashKey, transformInDate(rescheduleTime) )
+            }
+        }
+        pivotTimeInMillis = removeSingleMinute(pivotTimeInMillis)
+        issuersToRenew = await AllowedIssuerDao.listJwksCacheExpiringAtMinute( transformInDate(pivotTimeInMillis) )
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'OK' }),
+    }
+}
+
+function removeSingleMinute(date) {
+    return date - (1000*60)
+}
+
+function transformInDate(dateInMillis) {
+    let date = new Date(dateInMillis).toISOString();
+    return  date.slice(0, 16) + 'Z';
 }
 
 module.exports = { handleEvent };
