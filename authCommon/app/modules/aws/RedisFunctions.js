c6e42eca19a4f04d34cf56aadd1d4ed4b3b95aa3
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-06-05 22:31:33 +0200

    PN-11200: fixing

--- authCommon/app/modules/aws/RedisFunctions.js
+++ authCommon/app/modules/aws/RedisFunctions.js
@@ -1,6 +1,5 @@
 // Create a service client module using ES6 syntax.
 const { getRedisClient } = require('./Clients');
-const { default: Redlock } = require("redlock");
 
 const INITIAL_LOCK_TTL_SEC = parseInt(process.env.JWKS_FORCE_REFRESH_LAMBDA_TIMEOUT_SECONDS) 
                            + parseInt(process.env.MAXIMUM_CLOCK_DRIFT_SEC);
@@ -20,9 +19,7 @@ async function connectRedis() {
 }
 
 async function lockFunction(iss){
-    const redlock = new Redlock([redisClient]);
     console.log("Acquiring lock on iss: " + iss)
-    console.log(INITIAL_LOCK_TTL_SEC)
     const result = await redisClient.set("b2bauth:" + iss, 'locked', { NX: true, EX: INITIAL_LOCK_TTL_SEC });
     console.log("Lock is " + result)
     return result === 'OK';
@@ -36,14 +33,7 @@ async function unlockFunction(iss){
 
 async function extendLockFunction(iss, extendTime) {
     console.log("Extending lock on iss: " + iss)
-    console.log(extendTime)
-    const test = await redisClient.get("b2bauth:" + iss)
-    console.log("COSA", test)
-    let p = await redisClient.ttl("b2bauth:" + iss);
-    console.log("before",p)
     const result = await redisClient.set("b2bauth:" + iss, 'locked', { XX: true, EX: extendTime });
-    p = await redisClient.ttl("b2bauth:" + iss);
-    console.log("after", p)
     return result === 1; // 1 if the timeout was set, 0 if the key does not exist
 }
 
