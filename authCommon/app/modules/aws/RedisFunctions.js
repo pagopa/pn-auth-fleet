6a15ae4c23135f37144f8d1deea9c76ebc92f2d5
Vincenzo.Veniero <vincenzo.veniero@pagopa.it>
2024-06-06 22:29:35 +0200

    PN-11200: unit test

--- authCommon/app/modules/aws/RedisFunctions.js
+++ authCommon/app/modules/aws/RedisFunctions.js
@@ -1,10 +1,9 @@
 // Create a service client module using ES6 syntax.
 const { getRedisClient } = require('./Clients');
-
+const { REDIS_KEY } = require('./constants');
 const INITIAL_LOCK_TTL_SEC = parseInt(process.env.JWKS_FORCE_REFRESH_LAMBDA_TIMEOUT_SECONDS) 
                            + parseInt(process.env.MAXIMUM_CLOCK_DRIFT_SEC);
 
-const KEY_START = "b2bauth::"        
 let redisClient;
 
 async function connectRedis() {
@@ -17,18 +16,18 @@ async function connectRedis() {
 
 async function lockFunction(iss, value){
     console.log("Acquiring lock on iss: " + iss)
-    const result = await redisClient.set(KEY_START + iss, value, { NX: true, EX: INITIAL_LOCK_TTL_SEC});
+    const result = await redisClient.set(REDIS_KEY + iss, value, { NX: true, EX: INITIAL_LOCK_TTL_SEC});
     console.log("Lock is " + result)
     return result === 'OK';
 }
 
 async function unlockFunction(iss, value){
     console.log("Releasing lock on iss: " + iss)
-    const tmp = await redisClient.get(KEY_START + iss)
+    const tmp = await redisClient.get(REDIS_KEY + iss)
     let result
     if(tmp) {
         if(tmp == value) {
-            result = await redisClient.del(KEY_START + iss);
+            result = await redisClient.del(REDIS_KEY + iss);
             console.log("Release lock is " + result)
         }
         else {
@@ -42,7 +41,7 @@ async function unlockFunction(iss, value){
 
 async function extendLockFunction(iss, value, extendTime) {
     console.log("Extending lock on iss: " + iss)
-    let result = await redisClient.set(KEY_START + iss, value, { XX: true, EX: extendTime});
+    let result = await redisClient.set(REDIS_KEY + iss, value, { XX: true, EX: extendTime});
     console.log("Extend result " + result)
 }
 
