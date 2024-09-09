50b25cf0c11fd3d8dd292b10c624b08e3932806d
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-09-09 18:45:52 +0200

    feat: added multilayer for jwksCache* lambdas feat: added metrics for jwksCacheForceRefreshLambda

--- jwksCacheForceRefreshLambda/src/app/eventHandler.js
+++ jwksCacheForceRefreshLambda/src/app/eventHandler.js
@@ -3,22 +3,31 @@ const { RedisHandler, AllowedIssuerDao, UrlDownloader } = require('pn-auth-commo
 
 const intervalBetweenForcedRefreshSec = parseInt(process.env.MINIMUM_INTERVAL_BETWEEN_FORCED_REFRESH_SEC)
 
+const { MetricsHandler } = require('pn-auth-common');
+
+const metricsHandler = new MetricsHandler();
+
+
 async function handleEvent(event) {
-    for( const record of event.Records ) {
+    for (const record of event.Records) {
 
         const msgBodyStr = record.body;
-        const msg = JSON.parse( msgBodyStr );
+        const msg = JSON.parse(msgBodyStr);
         let issToRefresh = msg.iss;
-        let redisValue = Date.now() + "#"+ msg.uuid;
+        let redisValue = Date.now() + "#" + msg.uuid;
 
         console.log("request uuid: " + redisValue)
         await RedisHandler.connectRedis()
         try {
             let lock = await RedisHandler.lockFunction(issToRefresh, redisValue)
             // Perform Redis operations
-            if(lock) {
+            if (lock) {
                 await AllowedIssuerDao.addJwksCacheEntry(issToRefresh, UrlDownloader.downloadUrl)
-                await RedisHandler.extendLockFunction(issToRefresh, redisValue, intervalBetweenForcedRefreshSec)
+                await RedisHandler.extendLockFunction(issToRefresh, redisValue, intervalBetweenForcedRefreshSec);
+                const issData = getConfigByISS(iss);
+                const renewTimeMetricValue = Math.floor(Date.now() / 1000) - issData.jwksCacheOriginalExpireEpochSeconds
+                const metric = prepareJWKSRenewTimeMetric(issToRefresh, renewTimeMetricValue);
+                metricsHandler.addMetric(metric.metricName, metric.unit, metric.value, metric.dimension, metric.metadata);
             }
             else {
                 console.log("Lock already exists for issuer " + issToRefresh)
@@ -29,14 +38,15 @@ async function handleEvent(event) {
             throw error;
         } finally {
             await RedisHandler.disconnectRedis();
+            metricsHandler.publishMetrics();
         }
     }
-  
+
     const response = {
-      statusCode: 200,
-      body: JSON.stringify('Redis operation completed successfully!'),
+        statusCode: 200,
+        body: JSON.stringify('Redis operation completed successfully!'),
     };
     return response;
-  }
+}
 
 module.exports = { handleEvent };
