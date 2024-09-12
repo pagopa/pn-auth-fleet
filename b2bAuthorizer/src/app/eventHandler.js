a6656da33e91b0b3ebe3420c1c2b3453d9ac6875
abertu75 <122233952+abertu75@users.noreply.github.com>
2024-09-12 18:32:25 +0200

    Feature/PN-11659 (#261)
    * feat: added multilayer for jwksCache* lambdas
    feat: added metrics for jwksCacheForceRefreshLambda
    
    * added to previous commit
    
    * feat: added forced refresh metric
    
    * feat: added refresh metric management
    
    * fix: resolved comment
    
    * chore: updated package-lock.json

--- b2bAuthorizer/src/app/eventHandler.js
+++ b2bAuthorizer/src/app/eventHandler.js
@@ -4,7 +4,7 @@ const Logger = require("./modules/logger");
 const JwtService = require("./modules/jwt");
 const PolicyService = require("./modules/policy");
 const AuthenticationError = require("./errors/AuthenticationError");
-const { MetricsHandler } = require('pn-auth-common');
+const { MetricsHandler, JwtIssuerRefreshDTO } = require('pn-auth-common');
 const { prepareMetric, prepareMetricsJwtData } = require('./modules/metric/metricsUtils')
 
 // cache initialization
@@ -90,7 +90,7 @@ async function handleEvent(event) {
     }
     catch (err) {
       if(err instanceof AuthenticationError && err.retriable){
-        issuerInfo = await issuersCache.getWithForceRefresh( issuerId )
+        issuerInfo = await issuersCache.getWithForceRefresh(issuerId,  issuerInfo.cfg.jwksCacheOriginalExpireEpochSeconds);
         logger.addToContext('issuerInfo', issuerInfo);
         jwtService.validateToken( issuerInfo, decodedJwtToken, jwtToken, event );  // throw AutenticationError if something goes wrong
       } else {
