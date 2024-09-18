f18a526365b374381b494c7de4f686277378fe73
Andrea Bertucci <andrea.bertucci@pagopa.it>
2024-09-18 18:08:16 +0200

    fix: presi in carico i commenti sulla PR precedente

--- raddBaselining/src/app/eventHandler.js
+++ raddBaselining/src/app/eventHandler.js
@@ -8,53 +8,58 @@ const MS_IN_DAY = 86400000;
 const adjustDate = (d, days) => {
   if (!d)
     return;
- let millisToSec = d.getTime();
+  let millisToSec = d.getTime();
   if (days)
     millisToSec -= days * MS_IN_DAY;
   return millisToSec;
-} 
+}
 
-async function eventHandler( event ) {
+async function leastRecentAttributeEntry(issuer, resolver) {
+  const res = await JwtAttributesDao.listJwtAttributesByIssuer(issuer, resolver);
+  if (res.hasOwnProperty('modificationTimeEpochMs'))
+    return res.modificationTimeEpochMs;
+  else return Number.MAX_SAFE_INTEGER;
+}
+
+async function eventHandler(event) {
   const baseliningDeadline = adjustDate(new Date(), process.env.MAX_ATTRIBUTES_AGE_DAYS);
 
   let needBaselineCount = 0;
   let lastKey = undefined;
-  do {
-      const result = await AllowedIssuerDao.listRaddIssuers(lastKey);      
+  try {
+    do {
+      const result = await AllowedIssuerDao.listRaddIssuers(lastKey);
 
       for (const iss of result.Items) {
         const leastRecentDatabaseAttributeTs = await leastRecentAttributeEntry(iss, COMMON_COSTANTS.RADD_RESOLVER_NAME);
-      
-        if( leastRecentDatabaseAttributeTs < baseliningDeadline ) {
+
+        if (leastRecentDatabaseAttributeTs < baseliningDeadline) {
           // - Effettuare log a warning dell'elenco degli issuer 
           //   di cui è necessario il baselining (issuerToBeBaselined)
           console.warn("[RADD_BASELINING]", "Baselining needed for", iss);
           needBaselineCount++;
+        }
       }
-    }
-    lastKey = result.lastEvaluatedKey;
-  } while(lastKey);
-    
-    
-    
+      lastKey = result.lastEvaluatedKey;
+    } while (lastKey);
+  }
+  catch(e){
+    console.error("[RADD_BASELINING]", e);
+  }
+  finally {
+
     // // - Emettere metrica con 
     // //   Name: “JWKS_issuer_baselining_needed”
     // //   Value: issuerToBeBaselined.size()
     // // - Effettuare flush delle metriche
-    if(needBaselineCount != 0) {
-        metricsHandler.addMetric(METRIC_NAME, 'count', needBaselineCount);
-        metricsHandler.publishMetrics();
+    if (needBaselineCount != 0) {
+      metricsHandler.addMetric(METRIC_NAME, 'count', needBaselineCount);
+      metricsHandler.publishMetrics();
     }
   }
-  
-  
-  async function leastRecentAttributeEntry( issuer, resolver ) {
-    const res = await JwtAttributesDao.listJwtAttributesByIssuer( issuer, resolver );
-    if (res.hasOwnProperty('modificationTimeEpochMs'))
-        return res.modificationTimeEpochMs;
-    else return Number.MAX_SAFE_INTEGER;
-    
-     
-  }
-  
-  module.exports = { eventHandler };
+}
+
+
+
+
+module.exports = { eventHandler };
