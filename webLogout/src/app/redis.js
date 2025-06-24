d19b9d30b359a1a322f95d2fd0872f522ce22b1e
Francesco Bianchi <f.bianchi@codermine.com>
2025-06-24 11:50:10 +0200

    don't disconnect redis in lambda

--- webLogout/src/app/redis.js
+++ webLogout/src/app/redis.js
@@ -2,21 +2,18 @@ const { RedisClient } = require('pn-auth-common');
 const { REDIS_JTI_EXP, REDIS_JTI_PREFIX } = require("./constants");
 
 const insertJti = async (jti) => {
-  let client;
-
   try {
-    client = await RedisClient.getRedisClient();
-    await client.connect();
+    const client = await RedisClient.getRedisClient();
+    if (!client.isReady) {
+      await client.connect();
+    }
     await client.set(`${REDIS_JTI_PREFIX}${jti}`, "1", {
       EX: REDIS_JTI_EXP,
     });
   } catch (error) {
     throw error;
-  } finally {
-    if (client) {
-      await client.disconnect();
-    }
   }
+  // do not disconnect the client here, as it may be reused later
 };
 
 module.exports = {
