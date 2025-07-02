05c9699acb3370dc5eea5d2e402a278b827aa366
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2025-07-02 11:51:24 +0200

    Fix(PN-15323): object role in token "organization" (#325)

--- webLogout/src/test/utils.test.js
+++ webLogout/src/test/utils.test.js
@@ -2,13 +2,13 @@ const { getCxType, getCxId, getCxRole } = require("../app/utils");
 
 describe("getCxType", () => {
   it("should return 'PA' when token has organization", async () => {
-    const token = { organization: { id: "123", roles: [{ role: "admin" }] } };
+    const token = { organization: { id: "123", role: "admin" } };
     const result = await getCxType(token);
     expect(result).toEqual("PA");
   });
 
   it("should return 'PG' when token.organization.roles[0].role starts with 'pg-'", async () => {
-    const token = { organization: { roles: [{ role: "pg-manager" }] } };
+    const token = { organization: { role: "pg-manager" } };
     const result = await getCxType(token);
     expect(result).toEqual("PG");
   });
@@ -36,8 +36,18 @@ describe("getCxId", () => {
 
 describe("getCxRole", () => {
   it("should return the role if present", async () => {
-    const token = { organization: { roles: [{ role: "admin" }] } };
+    const token = { organization: { role: "admin" } };
     const result = await getCxRole(token);
     expect(result).toEqual("admin");
+  });
+  it("should return undefined if organization is missing", async () => {
+    const token = {};
+    const result = await getCxRole(token);
+    expect(result).toBeUndefined();
+  });
+  it("should return undefined if organization.role is missing", async () => {
+    const token = { organization: {} };
+    const result = await getCxRole(token);
+    expect(result).toBeUndefined();
   });
 });
