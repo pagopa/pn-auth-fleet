007806356fd289538debedc8291c8a626b046aa3
Francesco Bianchi <f.bianchi@codermine.com>
2025-07-02 14:45:31 +0200

    remove log

--- jwtAuthorizer/src/app/redis.js
+++ jwtAuthorizer/src/app/redis.js
@@ -1,15 +1,14 @@
 const { RedisHandler, COMMON_CONSTANTS } = require("pn-auth-common");
 
-/** 
+/**
  * @param {string} jti - The JWT ID to check for revocation.
  * @returns {Promise<boolean>} - Returns true if the JTI is revoked, false otherwise.
  */
 const isJtiRevoked = async (jti) => {
   try {
     await RedisHandler.connectRedis();
-    console.log("Connected to Redis for JTI revocation check key:", `${COMMON_CONSTANTS.REDIS_PN_SESSION_PREFIX}${jti}`);
+
     const isRevoked = await RedisHandler.get(`${COMMON_CONSTANTS.REDIS_PN_SESSION_PREFIX}${jti}`);
-    console.log("isRevoked:", isRevoked); 
     return !!isRevoked;
   } catch (error) {
     console.error("Error checking JTI revocation:", error);
