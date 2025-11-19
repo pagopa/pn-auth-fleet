5c0826a9b5683f44d96fb4b11b3a9747dac767b3
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2025-11-19 14:39:54 +0100

    PN-17273 - Validazione Assertion Period (notBefore) - correct test

--- lollipopAuthorizer/src/test/assertionValidation.test.js
+++ lollipopAuthorizer/src/test/assertionValidation.test.js
@@ -6,33 +6,41 @@ const { VALIDATION_ERROR_CODES } = require('../app/constants/lollipopConstants')
 describe('validateAssertionPeriodTest ', () => {
     console.log('validateAssertionPeriod TEST');
 
-    try{
-        validateAssertionPeriod("2025-11-04T11:50:33.570Z");
-        console.log('notBefore is VALID');
-    } catch(err) {
-        expect(err).to.be.instanceOf(LollipopAssertionException);
-        expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
-     }
+    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', async () => {
+        try{
+            validateAssertionPeriod("2025-11-04T11:50:33.570Z");
+            console.log('notBefore is VALID');
+        } catch(err) {
+            expect(err).to.be.instanceOf(LollipopAssertionException);
+            expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
+        }
+    });
 
-    try{
-        validateAssertionPeriod("2025-11-04T13.570Z");
-    } catch(err) {
-       expect(err).to.be.instanceOf(LollipopAssertionException);
-       expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
-    }
+    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', async () => {
+        try{
+            validateAssertionPeriod("2025-11-04T13.570Z");
+        } catch(err) {
+           expect(err).to.be.instanceOf(LollipopAssertionException);
+           expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
+        }
+    });
 
-     try{
-         validateAssertionPeriod('');
-     } catch(err) {
-        expect(err).to.be.instanceOf(LollipopAssertionException);
-        expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
-     }
+    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', async () => {
+         try{
+             validateAssertionPeriod('');
+         } catch(err) {
+            expect(err).to.be.instanceOf(LollipopAssertionException);
+            expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
+         }
+     });
 
-     try{
-         validateAssertionPeriod("2020-11-04T11:50:33.570Z");
-     } catch(err) {
-        expect(err).to.be.instanceOf(LollipopAssertionException);
-        expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.NOTBEFORE_NOTVALID_DATE);
-     }
+    it('should throw NOTBEFORE_NOTVALID_DATE if notBefore is not valid', async () => {
+         try{
+             validateAssertionPeriod("2020-11-04T11:50:33.570Z");
+         } catch(err) {
+            expect(err).to.be.instanceOf(LollipopAssertionException);
+            expect(err.errorCode).to.be.equal(VALIDATION_ERROR_CODES.NOTBEFORE_NOTVALID_DATE);
+         }
+     });
 });
 
