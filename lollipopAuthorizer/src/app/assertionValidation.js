56e39d05a5945b5410507e8800295d04de9dc33f
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2025-11-20 14:55:57 +0100

    PN-17273 - rewiev dei test

--- lollipopAuthorizer/src/app/assertionValidation.js
+++ lollipopAuthorizer/src/app/assertionValidation.js
@@ -1,12 +1,27 @@
+const xmldom = require('xmldom');
 const LollipopRequestContentValidationException = require('../app/exception/lollipopRequestContentValidationException');
-const { VALIDATION_ERROR_CODES, ASSERTION_EXPIRE_IN_DAYS, MILLISECONDS_PER_DAY } = require('../app/constants/lollipopConstants');
+const { VALIDATION_ERROR_CODES, SAML_ASSERTION, ASSERTION_EXPIRE_IN_DAYS, MILLISECONDS_PER_DAY } = require('../app/constants/lollipopConstants');
 const LollipopAssertionException = require('../app/exception/lollipopAssertionException');
 
 
- function validateAssertionPeriod(notBefore){
-    //notBefore è nel formato 2025-11-04T11:50:33.570Z
+ function validateAssertionPeriod(assertionDoc){
+
+    const rootElementName = assertionDoc.documentElement.localName;
+    const listElements = assertionDoc.getElementsByTagNameNS(SAML_ASSERTION.SAML2_ASSERTION_NS, SAML_ASSERTION.NOT_BEFORE_TAG);
+    if( isElementNotFound(listElements, SAML_ASSERTION.NOT_BEFORE) ) {
+        console.error('[validateAssertionPeriod] The notBefore parameter is not valid or an error occurred during parsing');
+        throw new LollipopAssertionException(
+            VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE,
+            'The notBefore parameter is not valid or an error occurred during parsing'
+        );
+    }
+    const firstConditionsElement = listElements[0];
+    const notBefore = firstConditionsElement.getAttribute(SAML_ASSERTION.NOT_BEFORE);
+    console.debug("notBefore: ", notBefore);
+    //notBefore deve essere nel formato 2025-11-04T11:50:33.570Z
     const notBeforeMilliseconds = Date.parse(notBefore);
-    if(isNaN(notBeforeMilliseconds)) {
+    console.debug("notBeforeMilliseconds: ", notBeforeMilliseconds);
+    if( isNaN(notBeforeMilliseconds)) {
          console.error('[validateAssertionPeriod] The notBefore parameter is not valid or an error occurred during parsing');
          throw new LollipopAssertionException(
            VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE,
@@ -30,6 +45,22 @@ const LollipopAssertionException = require('../app/exception/lollipopAssertionEx
     const isNotExpired = (dateNowLessNotBefore <= expiresAfterMilliseconds);
     return (isNotBeforeValid && isNotExpired);
  }
+
+ function isElementNotFound(listElements, elementName) {
+     if (!listElements || listElements.length === 0) {
+         return true;
+     }
+     const firstElement = listElements[0];
+     if (!firstElement) {
+         return true;
+     }
+     const attributeValue = firstElement.getAttribute(elementName);
+     if (!attributeValue) {
+         return true;
+     }
+     return false;
+ }
+
 
  module.exports = {
   validateAssertionPeriod,
--- lollipopAuthorizer/src/test/assertionValidation.test.js
+++ lollipopAuthorizer/src/test/assertionValidation.test.js
@@ -1,36 +1,130 @@
 const {expect} = require('chai');
 const chai = require('chai');
-const { validateAssertionPeriod, LollipopAssertionException } = require('../app/assertionValidation');
-const { VALIDATION_ERROR_CODES } = require('../app/constants/lollipopConstants');
+const xmldom = require('xmldom');
+const path = require('path');
+const fs = require('fs').promises;
+const { validateAssertionPeriod } = require('../app/assertionValidation');
+const { VALIDATION_ERROR_CODES, SAML_ASSERTION } = require('../app/constants/lollipopConstants');
+const LollipopAssertionException = require('../app/exception/lollipopAssertionException');
+
 
 describe('validateAssertionPeriodTest ', () => {
+
     console.log('validateAssertionPeriod TEST');
 
-    it('dovrebbe restituire TRUE quando la data è valida e non è scaduta', () => {
-        const notBefore = "2025-11-04T11:50:33.570Z";
-        const result = validateAssertionPeriod(notBefore);
+    it('dovrebbe restituire TRUE quando la data è valida e non è scaduta', async () => {
+
+        console.log('TEST 1 - Caricamento Document Assertion fake per test ');
+        let assertionDoc;
+        try {
+            const getAssertionXMLPath = path.resolve('C://PROGETTI_DGS//PAGO_PA//REPOSITORIES//pagopa//pn-auth-fleet//lollipopAuthorizer//src//test//fileTest//getAssertionTest.xml');
+            console.log("gestAssertionPath: " , getAssertionXMLPath);
+            const getAssertionXmlString = await fs.readFile(getAssertionXMLPath, 'utf8');
+            const parserXML = new xmldom.DOMParser();
+            assertionDoc = parserXML.parseFromString(getAssertionXmlString, "application/xml");
+            if (assertionDoc.documentElement.nodeName === 'parsererror') {
+                throw new Error("Errore di sintassi nel file XML. Parsing fallito.");
+            }
+            console.log("TYPE assertionDoc: ", typeof assertionDoc);
+            const rootElementName = assertionDoc.documentElement.localName;
+            console.log("assertionDoc di test - rootElementName: " , rootElementName);
+
+        } catch (error) {
+            console.error(`Errore durante il caricamento da file XML: ${error.message}`);
+            throw error;
+        }
+
+        if(!assertionDoc  || assertionDoc === undefined){
+            console.error("test assertionDoc is NULL");
+        }else{
+            console.debug("test assertionDoc is NOT NULL");
+            const listElements = assertionDoc.getElementsByTagNameNS(SAML_ASSERTION.SAML2_ASSERTION_NS, SAML_ASSERTION.NOT_BEFORE_TAG);
+            const firstConditionsElement = listElements[0];
+            const notBefore = firstConditionsElement.getAttribute(SAML_ASSERTION.NOT_BEFORE);
+            console.debug("notBefore: ", notBefore);
+        }
+
+        console.log("validateAssertionPeriod ...");
+        const result = validateAssertionPeriod(assertionDoc);
         expect(result).to.be.true;
     });
 
-    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', () => {
-        const notBeforeIsNotValidParsing = "2025-11-04T13.570Z";
-        expect(() => validateAssertionPeriod(notBeforeIsNotValidParsing))
+    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', async() => {
+
+        console.log('TEST 2 - Caricamento Document Assertion fake per test ');
+        let assertionDocNotBeforeNull;
+        try {
+            const getAssertionXMLPath = path.resolve('C://PROGETTI_DGS//PAGO_PA//REPOSITORIES//pagopa//pn-auth-fleet//lollipopAuthorizer//src//test//fileTest//getAssertionTestNotBeforeInvalid.xml');
+
+            console.log("gestAssertionPath: " , getAssertionXMLPath);
+            const getAssertionXmlString = await fs.readFile(getAssertionXMLPath, 'utf8');
+            const parserXML = new xmldom.DOMParser();
+            assertionDocNotBeforeNull = parserXML.parseFromString(getAssertionXmlString, "application/xml");
+            if (assertionDocNotBeforeNull.documentElement.nodeName === 'parsererror') {
+                throw new Error("Errore di sintassi nel file XML. Parsing fallito.");
+            }
+
+        } catch (error) {
+            console.error(`Errore durante il caricamento da file XML: ${error.message}`);
+            throw error;
+        }
+
+        if(!assertionDocNotBeforeNull  || assertionDocNotBeforeNull === undefined){
+            console.error("test 2 assertionDoc is NULL");
+        }else{
+            console.debug("test 2 assertionDoc is NOT NULL");
+            const listElements = assertionDocNotBeforeNull.getElementsByTagNameNS(SAML_ASSERTION.SAML2_ASSERTION_NS, SAML_ASSERTION.NOT_BEFORE_TAG);
+            const firstConditionsElement = listElements[0];
+            const notBefore = firstConditionsElement.getAttribute(SAML_ASSERTION.NOT_BEFORE);
+            console.debug("notBefore:", notBefore);
+        }
+
+        expect(() => validateAssertionPeriod(assertionDocNotBeforeNull))
         .to.throw(LollipopAssertionException)
         .with.property('errorCode', VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
-    });
 
-    it('should throw ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE if notBefore is null or invalid', () => {
-        const notBeforeIsNull = "";
-        expect(() => validateAssertionPeriod(notBeforeIsNull))
-        .to.throw(LollipopAssertionException)
-        .with.property('errorCode', VALIDATION_ERROR_CODES.ERROR_PARSING_ASSERTION_NOT_BEFORE_DATE);
-    });
+//            const result = validateAssertionPeriod(assertionDocNotBeforeNull);
+//            expect(result).to.be.false;
 
-    it('dovrebbe restituire FALSE quando la data è valida ma è scaduta', () => {
-        const notBeforeIsNotValid = "2020-11-04T11:50:33.570Z";
-        const result = validateAssertionPeriod(notBeforeIsNotValid);
+      });
+
+    it('dovrebbe restituire FALSE quando la data è valida e non è scaduta', async () => {
+
+        console.log('TEST 1 - Caricamento Document Assertion fake per test ');
+        let assertionDoc;
+        try {
+            const getAssertionXMLPath = path.resolve('C://PROGETTI_DGS//PAGO_PA//REPOSITORIES//pagopa//pn-auth-fleet//lollipopAuthorizer//src//test//fileTest//getAssertionTestNotBeforeFalse.xml');
+            console.log("gestAssertionPath: " , getAssertionXMLPath);
+            const getAssertionXmlString = await fs.readFile(getAssertionXMLPath, 'utf8');
+            const parserXML = new xmldom.DOMParser();
+            assertionDoc = parserXML.parseFromString(getAssertionXmlString, "application/xml");
+            if (assertionDoc.documentElement.nodeName === 'parsererror') {
+                throw new Error("Errore di sintassi nel file XML. Parsing fallito.");
+            }
+            console.log("TYPE assertionDoc: ", typeof assertionDoc);
+            const rootElementName = assertionDoc.documentElement.localName;
+            console.log("assertionDoc di test - rootElementName: " , rootElementName);
+
+        } catch (error) {
+            console.error(`Errore durante il caricamento da file XML: ${error.message}`);
+            throw error;
+        }
+
+        if(!assertionDoc  || assertionDoc === undefined){
+            console.error("test assertionDoc is NULL");
+        }else{
+            console.debug("test assertionDoc is NOT NULL");
+            const listElements = assertionDoc.getElementsByTagNameNS(SAML_ASSERTION.SAML2_ASSERTION_NS, SAML_ASSERTION.NOT_BEFORE_TAG);
+            const firstConditionsElement = listElements[0];
+            const notBefore = firstConditionsElement.getAttribute(SAML_ASSERTION.NOT_BEFORE);
+            console.debug("notBefore is OLD: ", notBefore);
+        }
+
+        console.log("validateAssertionPeriod ...");
+        const result = validateAssertionPeriod(assertionDoc);
         expect(result).to.be.false;
     });
 
 });
+
 
