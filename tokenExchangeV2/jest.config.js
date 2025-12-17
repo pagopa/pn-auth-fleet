322dee837b4045ed1b6225d8a5c54667ec0af667
alessandrogelmi <ale.gelmi2@gmail.com>
2025-12-17 16:55:08 +0100

    feat: add responses and utility tests

--- tokenExchangeV2/jest.config.js
+++ tokenExchangeV2/jest.config.js
@@ -5,6 +5,7 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  setupFiles: ["<rootDir>/jest.setup.env.js"],
   transform: {
     ...tsJestTransformCfg,
   },
