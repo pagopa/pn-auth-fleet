bce11c68433f24e9c4c9773c22069e994b2429fa
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2025-12-22 12:30:27 +0100

    PN-17286 - lambda develop

--- lollipopAuthorizer/src/app/eventHandler.js
+++ lollipopAuthorizer/src/app/eventHandler.js
@@ -1,5 +1,8 @@
-const { validateLollipopRequestAssertion } = require('../app/LollipopAssertionValidation');
+const { validateLollipopRequest } = require("../app/lollipopRequestValidation");
+//const { validateLollipopHttpSignature } = require("../app/lollipopHttpSignatureValidation");
+const { validateLollipopAssertion } = require('../app/lollipopAssertionValidation');
 const LollipopRequestContentValidationException = require('../app/exception/lollipopRequestContentValidationException');
+const LollipopHttpSignatureValidationException = require('../app/exception/lollipopHttpSignatureValidationException');
 const LollipopAssertionException = require('../app/exception/LollipopAssertionException');
 const { generateIAMPolicy } = require("./iamPolicyGen.js");
 
@@ -19,100 +22,112 @@ const defaultDenyAllPolicy = {
 
 async function handleEvent(event) {
 
-    //if (event.headers.authorization == "secretToken") {
-        console.log("allowed");
-        let commandResult;
-        try {
-              const request = {
-                // Mapping degli header ricevuti dall'evento
-                headerParams: {
-                    headers: event.headers || {}
-                },
-              };
+    console.log("[handleEvent] Lollipop Authorizer Validation Allowed");
+    let commandResult;
+    let commandResultName ='';
+    let commandResultFamilyName='';
+    try {
+          const request = {
+            // Mapping degli header ricevuti dall'evento
+            headerParams: {
+                headers: event.headers || {}
+            },
+          };
 
-              commandResult = await validationRequestAssertion(request);
+          commandResult = await validateLollipopAuthorizer(request);
 
-              // Capture taxId from event
-              const taxId = event?.headers?.["x-pagopa-cx-taxid"];
-              if (!taxId) {
-                  console.error("Header 'x-pagopa-cx-taxid' is missing. Denying access.");
-                  return defaultDenyAllPolicy;
-              }
-              const cxId = await getCxId(taxId);
-              console.info("cxId", cxId);
-                //if(commandResult
-              const contextMap = {
-                  name: commandResult.name,
-                  familyName: commandResult.familyName,
-                  cxId: cxId,
-              };
+          // Capture taxId from event
+          const taxId = event?.headers?.["x-pagopa-cx-taxid"];
+          if (!taxId) {
+              console.error("Header 'x-pagopa-cx-taxid' is missing. Denying access.");
+              return defaultDenyAllPolicy;
+          }
+          const cxId = await getCxId(taxId);
+          console.log("cxId", cxId);
 
-              // Generate IAM Policy
-              const iamPolicy = await generateIAMPolicy(event.methodArn, cxId, contextMap);
-              console.log("IAM Policy", JSON.stringify(iamPolicy));
-              return iamPolicy;
-        } catch (error) {
-            console.error("Error during authorization flow (get/generate policy): ", error);
-            // Se l'errore è un "Unauthorized" lanciato precedentemente, API Gateway lo gestisce.
-            // Se è un errore del server (es. getCxId fallisce), neghiamo l'accesso per sicurezza.
-            if (error.message === "Unauthorized") {
-                // Rilancia l'errore per far sì che API Gateway restituisca 401/403
-                throw error;
-            }
-            // Errore interno al server (500), nega l'accesso
-            return defaultDenyAllPolicy;
-        }
-   /* }else{
-        console.log("denied");
+          const contextMap = {
+              name: commandResult.name || '',
+              familyName: commandResult.familyName || '',
+              cxId: cxId,
+          };
+
+          // Generate IAM Policy
+          const iamPolicy = await generateIAMPolicy(event.methodArn, cxId, contextMap);
+          console.debug("IAM Policy ", JSON.stringify(iamPolicy));
+          return iamPolicy;
+    } catch (error) {
+        console.error("Lollipop Authorizer Validation - Error during authorization flow (get/generate policy): ", error);
         return defaultDenyAllPolicy;
-    }*/
+    }
 }
 
 
-    async function validationRequestAssertion(request){
+    async function validateLollipopAuthorizer(request){
+        console.log("[validateLollipopAuthorizer] - starting ...");
 
-        let commandResult;
+        let commandResult = { statusCode: 500, resultCode: "INIT", resultMessage: "" };
         try {
-            //Validazione degli header per una richiesta Lollipop.
+            //STEP 1- Validazione degli header per una richiesta Lollipop.
             await validateLollipopRequest(request);
 
-            //Validazione della Signature
-            //await validateSignatureAssertion();
-
-            //Validazione della assertion della request Lollipop.
-            commandResult = await validateLollipopRequestAssertion(request);
+            //STEP 2 - Validazione della Http Signature
+//            commandResult = await validateLollipopHttpSignature(request);
+            if(commandResult.resultCode !== "HTTP_MESSAGE_VALIDATION_SUCCESS"){
+                commandResult.statusCode = 402;
+                console.error("Lollipop Request Http Signature Validation failed - statusCode: ", commandResult.statusCode,
+                        " - resultCode: ", commandResult.resultCode, " - resultMessage: ", commandResult.resultMessage);
+                return commandResult;
+            }
+            //STEP 3 - Validazione della assertion della request Lollipop.
+            commandResult = await validateLollipopAssertion(request);
 
             // Se la validazione è completata con successo
-            commandResult.statusCode = 200;
+            if(commandResult.resultCode === "ASSERTION_VERIFICATION_SUCCESS"){
+                commandResult.statusCode = 200;
+            }
+            console.log("[validateLollipopAuthorizer] - ending statusCode: ", commandResult.statusCode,
+                      " - resultCode: ", commandResult.resultCode, " - resultMessage: ", commandResult.resultMessage);
+
             return commandResult;
+
         } catch (error) {
-            console.error("Lollipop Request Assertion Validation failed:", error.name, error.message);
+            console.error("Lollipop Authorizer Validation failed:", error.name, error.message);
             // Gestione degli errori
             let statusCode = 500;
-            let resultMessage;
+            let resultCode = "FATAL_ERROR";
+            let resultMessage = '';
             if (error instanceof LollipopRequestContentValidationException) {
                 // Bad Request per dati mancanti/invalidi
                 statusCode = 401;
                 resultCode = "REQUEST_PARAMS_VALIDATION_FAILED";
                 resultMessage = `Error validating Lollipop request header or body, validation failed`
-                          + ` with error code [${error.code}] and message: ${error.message}`;
-                console.error(resultMessage);
+                          + ` with error code [${error.errorCode}] and message: ${error.message}`;
+
+            }else if (error instanceof LollipopHttpSignatureValidationException) {
+                statusCode = 402;
+                resultCode = "HTTP_MESSAGE_VALIDATION_FAILED";
+                resultMessage = `Error validating Lollipop http Signature, validation failed`
+                          + ` with error code [${error.errorCode}] and message: ${error.message}`;
+
             }else if (error instanceof LollipopAssertionException) {
                 statusCode = 403;
                 resultCode = "REQUEST_ASSERTION_VALIDATION_FAILED";
                 resultMessage = `Error validating Lollipop request assertion, validation failed`
-                          + ` with error code [${error.code}] and message: ${error.message}`;
+                          + ` with error code [${error.errorCode}] and message: ${error.message}`;
 
             }else {
-                statusCode = 500;
-                resultCode = "FATAL_ERROR";
                 resultMessage = `Error validating Lollipop request assertion, validation failed`
-                          + ` with error code [${error.code}] and message: ${error.message}`;
+                          + ` with error code [${error.errorCode}] and message: ${error.message}`;
             }
-            commandResult.statusCode = statusCode;
-            commandResult.resultCode = resultCode;
-            commandResult.resultMessage = resultMessage;
-            return commandResult;
+
+            console.error("[validateLollipopAuthorizer] - ending statusCode: ", statusCode,
+                       " - resultCode: ", resultCode, " - resultMessage: ", resultMessage);
+
+            return {
+                statusCode,
+                resultCode,
+                resultMessage: `Validation failed [${error.errorCode || 'GENERIC'}]: ${error.message}`
+            };
         }
     }
 
