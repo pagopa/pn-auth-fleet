bbe03bed078b45ca6a7325fbdae4108c197af81a
Francesco Bianchi <f.bianchi@codermine.com>
2026-03-30 14:42:50 +0200

    create apigatewayclient with region

--- authCommon/app/modules/utils/apiGatewayUtils.js
+++ authCommon/app/modules/utils/apiGatewayUtils.js
@@ -1,7 +1,5 @@
 const { APIGatewayClient, GetTagsCommand } = require("@aws-sdk/client-api-gateway");
 
-const apigwClient = new APIGatewayClient();
-
 /**
  * Retrieves the PN tags from the specified API Gateway REST API.
  *
@@ -11,12 +9,13 @@ const apigwClient = new APIGatewayClient();
  * @returns {Promise<{bucket: string, key: string, servicePath: string, apiName: string}>}
  */
 async function getApiGatewayTags({ region, restApiId }) {
+  const apigwClient = new APIGatewayClient({ region });
   const input = {
     resourceArn: `arn:aws:apigateway:${region}::/restapis/${restApiId}`,
   };
   const command = new GetTagsCommand(input);
   const data = await apigwClient.send(command);
-  console.log("API Gateway tags:", data.tags);
+
   return {
     bucketName: data.tags.PN_OPENAPI_BUCKET_NAME,
     bucketKey: data.tags.PN_OPENAPI_BUCKET_KEY,
