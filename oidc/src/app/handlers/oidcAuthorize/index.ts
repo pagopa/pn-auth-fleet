dbaae0ad8488367436dfa53e94ac339b26403578
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2026-05-05 15:34:08 +0200

    handler oidc-state (#468)

--- oidc/src/app/handlers/oidcAuthorize/index.ts
+++ oidc/src/app/handlers/oidcAuthorize/index.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { RedisHandler } from "pn-auth-common";
 import { OneIdentityAwsSecretObject } from "../../models/Aws";
+import type { OidcStateData } from "../../models/OidcState";
 import { generateKoResponse, generateOkResponse } from "../../utils/Responses";
 import { generateRandomUniqueString, retrieveEnvVariable } from "../../utils/String";
 import { getAWSSecret } from "../oidcToken/utils/AwsParameters";
@@ -40,7 +41,11 @@ export const oidcAuthorizeHandler = async (event: APIGatewayProxyEvent): Promise
 
   await RedisHandler.connectRedis();
   try {
-    await RedisHandler.setJson(getOidcStateRedisKey(state), { nonce, idp, aar, retrievalId }, { EX: redisStateTtlSec });
+    await RedisHandler.setJson<OidcStateData>(
+      getOidcStateRedisKey(state),
+      { nonce, idp, aar, retrievalId },
+      { EX: redisStateTtlSec },
+    );
   } finally {
     await RedisHandler.disconnectRedis();
   }
