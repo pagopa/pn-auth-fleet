8ff163f995a848062f6978b3976be80700f3c01c
Francesco Bianchi <f.bianchi@codermine.com>
2026-07-28 16:47:41 +0200

    console.warn on disconnect redis error

--- jwtAuthorizer/src/app/redis.js
+++ jwtAuthorizer/src/app/redis.js
@@ -22,7 +22,7 @@ const disconnectRedis = async () => {
   try {
     await RedisHandler.disconnectRedis();
   } catch (error) {
-    console.error("Error disconnecting from Redis:", error);
+    console.warn("Error disconnecting from Redis:", error);
   }
 };
 
